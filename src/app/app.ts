89311448d8ad662536994359ecc3af5cb96a6f79
Tiago Segatti <tiago01segatti@gmail.com>
2026-05-03 19:20:24 -0300

    Creating card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
+import { ProductsGrid } from './produtcs/products-grid/products-grid';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ProductsGrid],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
