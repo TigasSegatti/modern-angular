e6deb499f0c34999402a1f0bd6009539c2288adb
Tiago Segatti <tiago01segatti@gmail.com>
2026-07-19 19:18:49 -0300

    Adicionando mensagem de vazio

--- src/app/produtcs/products-grid/products-grid.ts
+++ src/app/produtcs/products-grid/products-grid.ts
@@ -1,35 +1,36 @@
 import { Product } from './../product';
 import { Component, signal } from '@angular/core';
 import { ProductsCard } from '../products-card/products-card';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductsCard],
+  imports: [ProductsCard, MatIcon],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
 
   protected readonly products = signal<Product[]>([
-    {id: 1,
-    name: "Gaming Mouse RGB",
-    description: "Ergonomic gaming mouse with customizable RGB lighting and 7200 DPI sensor.",
-    price: 149.90,
-    originalPrice: 199.90,
-  },
-  {
-    id: 2,
-    name: "Mechanical Keyboard",
-    description: "Compact mechanical keyboard with red switches and white LED backlighting.",
-    price: 289.90,
-    originalPrice: 349.90,
-  },
-  {
-    id: 3,
-    name: "Wireless Headset",
-    description: "Wireless headset with noise-canceling microphone and up to 30 hours of battery life.",
-    price: 399.90,
-  },
+  //   {id: 1,
+  //   name: "Gaming Mouse RGB",
+  //   description: "Ergonomic gaming mouse with customizable RGB lighting and 7200 DPI sensor.",
+  //   price: 149.90,
+  //   originalPrice: 199.90,
+  // },
+  // {
+  //   id: 2,
+  //   name: "Mechanical Keyboard",
+  //   description: "Compact mechanical keyboard with red switches and white LED backlighting.",
+  //   price: 289.90,
+  //   originalPrice: 349.90,
+  // },
+  // {
+  //   id: 3,
+  //   name: "Wireless Headset",
+  //   description: "Wireless headset with noise-canceling microphone and up to 30 hours of battery life.",
+  //   price: 399.90,
+  // },
 
   ]);
 }
