0e96fe4dd32550f11f3ec3256d0f9fc824be4ac3
Tiago Segatti <tiago01segatti@gmail.com>
2026-07-19 18:51:12 -0300

    Adding itens in the products

--- src/app/produtcs/products-grid/products-grid.ts
+++ src/app/produtcs/products-grid/products-grid.ts
@@ -10,5 +10,26 @@ import { ProductsCard } from '../products-card/products-card';
 })
 export class ProductsGrid {
 
-  protected readonly products = signal<Product[]>([])
+  protected readonly products = signal<Product[]>([
+    {id: 1,
+    name: "Gaming Mouse RGB",
+    description: "Ergonomic gaming mouse with customizable RGB lighting and 7200 DPI sensor.",
+    price: 149.90,
+    originalPrice: 199.90,
+  },
+  {
+    id: 2,
+    name: "Mechanical Keyboard",
+    description: "Compact mechanical keyboard with red switches and white LED backlighting.",
+    price: 289.90,
+    originalPrice: 349.90,
+  },
+  {
+    id: 3,
+    name: "Wireless Headset",
+    description: "Wireless headset with noise-canceling microphone and up to 30 hours of battery life.",
+    price: 399.90,
+  },
+
+  ]);
 }
